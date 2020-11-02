1d02f6fb49a33a6780dc30f44852f1acc320b576
snappyyw <snippymr@yandex.ru>
2020-11-02 15:33:33 +0300

    Fomacum

--- scrypt.js
+++ scrypt.js
@@ -70,3 +70,79 @@ function glas(str){
     }
     return res;
 }
+
+class Auto{   
+    constructor(name, year, price){
+        this.name=name;
+        this.year=year;
+        this.price=price;
+    }
+    ride(){}
+    refuel(){}
+    getInfo(){
+        return this.name + " " + this.year + " " + this.price;
+    }
+    idOlderThan(year){
+        if(year>this.year){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
+
+class ElectricСar extends Auto{
+    constructor(name, year, price, battery){
+        super(name, year, price);
+        this.battery=battery;
+    }
+    ride(distance){
+        this.battery = this.battery - distance*5;
+        alert("Осталость заряда: " + this.battery);
+    }
+
+    refuel(charge){
+        this.battery = this.battery + charge;
+        alert("Теперь заряда: " + this.battery);
+    }
+    getInfo(){
+        alert(super.getInfo() + " " + this.battery);
+    }
+    idOlderThan(year){
+        if(super.idOlderThan(year)){
+            alert("Да");
+        }
+        else{
+            alert("Нет");
+        }
+    }
+}
+
+class DieselCar extends Auto{
+    constructor(name, year, price, tank){
+        super(name, year, price);
+        this.tank=tank;
+    }
+    ride(distance){
+        this.tank = this.tank - distance*3;
+        alert("Осталость в баке: " + this.tank)
+    }
+
+    refuel(fuel){
+        this.tank = this.tank + fuel;
+        alert("Теперь в баке: " + this.tank);
+    }
+    getInfo(){
+        alert(super.getInfo() + " " + this.tank);
+    }
+    idOlderThan(year){
+        if(super.idOlderThan(year)){
+            alert("Да");
+        }
+        else{
+            alert("Нет");
+        }
+    }
+}
+let ford = new DieselCar("Ford",2005,250000,115);
